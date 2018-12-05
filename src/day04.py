3e90757610827a109fcf96d04a049a17f3f42e03
AndreFCruz <andrecruz97@gmail.com>
2018-12-05 17:06:40 +0000

    day 4 complete

--- src/day04.py
+++ src/day04.py
@@ -2,8 +2,7 @@ import sys
 import re
 from datetime import datetime
 
-# lines = [l.rstrip() for l in sys.stdin.readlines()]
-lines = [l.rstrip() for l in open('input/day04.in').readlines()]
+lines = [l.rstrip() for l in sys.stdin.readlines()]
 matches = list(map(lambda l: re.match(r'\[(\d+)-(\d+)-(\d+) (\d+):(\d+)\] (Guard #(\d+) .*|falls .*|wakes .*)', l), lines))
 dates = {datetime(int(m.group(1)), int(m.group(2)), int(m.group(3)), int(m.group(4)), int(m.group(5))) : m for m in matches}
 
@@ -24,7 +23,14 @@ for date, match in sorted(dates.items()):
         for i in range(start, end):
             guards_schedule[guard_id][i] += 1
 
+## First part
 guard_most_asleep = max(guards_schedule, key=lambda k: sum(guards_schedule[k]))
 minute_most_asleep = guards_schedule[guard_most_asleep].index(max(guards_schedule[guard_most_asleep]))
 
 print(guard_most_asleep * minute_most_asleep)
+
+## Second part
+guard_most_freq_asleep = max(guards_schedule, key=lambda k: max(guards_schedule[k]))
+minute_most_freq_asleep = guards_schedule[guard_most_freq_asleep].index(max(guards_schedule[guard_most_freq_asleep]))
+
+print(guard_most_freq_asleep * minute_most_freq_asleep)
