b4ab8b87f1a6f9c97d25d6c3b22c10b7e5cb6a2b
AndreFCruz <andrecruz97@gmail.com>
2018-12-06 14:53:21 +0000

    more efficient day5 solution

--- src/day05.py
+++ src/day05.py
@@ -2,7 +2,7 @@ import sys
 
 line = sys.stdin.readline().rstrip()
 
-# O(n^2), as string manipulation is O(n) in the size of the string
+# O(n^2), as string manipulation is O(n)
 def perform_reactions(string):
     i = 0
     while i < len(string) - 1:
@@ -14,13 +14,38 @@ def perform_reactions(string):
 
     return string
 
-def shortest_polymer(string):
+# O(n) attempt by modifying the string in place instead of creating a new one at each iteration
+def perform_reactions_in_place(string):
+    def find_nearest(li, condition, start, end, step=1):
+        for i in range(start, end, step):
+            if condition(li[i]):
+                return i
+        return -1
+
+    s = [c for c in string] # Copy string to list, in order to modify in place
+    i = 0
+    while i < len(string) - 1:
+        nearest_base = find_nearest(s, lambda c: c != '.', start=i+1, end=len(s), step=1)
+        if nearest_base == -1:
+            break
+        if s[i] != '.' and s[i].upper() == s[nearest_base].upper() and s[i] != s[nearest_base]:
+            s[i], s[nearest_base] = '.', '.' # Mark to be removed
+            i = find_nearest(s, lambda c: c != '.', start=i-1, end=-1, step=-1)
+        else:
+            i = find_nearest(s, lambda c: c != '.', start=i+1, end=len(s), step=1)
+
+    return ''.join(filter(lambda c: c != '.', s))
+
+def shortest_polymer(string, perform_reactions_func=perform_reactions):
     lengths = list()
     for base in set(string.upper()):
         s = list(filter(lambda c: c.upper() != base, string))
-        lengths.append(len(perform_reactions(s)))
+        lengths.append(len(perform_reactions_func(s)))
         # print('base "{}": {}'.format(base, lengths[-1]))
     return min(lengths)
 
-print(len(perform_reactions(line)))
-print(shortest_polymer(line))
+# First part
+print(len(perform_reactions_in_place(line)))
+
+# Second part
+print(shortest_polymer(line, perform_reactions_in_place))
