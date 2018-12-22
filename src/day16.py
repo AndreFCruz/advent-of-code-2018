1669c02c09c5611f9d83ce2a347d5e3e09d21bda
AndreFCruz <andrecruz97@gmail.com>
2018-12-22 17:37:12 +0000

    day16 part 2

--- src/day16.py
+++ src/day16.py
@@ -1,16 +1,11 @@
 import sys, re
+from ortools.sat.python import cp_model
+from functools import reduce
 
 # Operation Indices
 OP_CODE_IDX, A_IDX, B_IDX, C_IDX = 0, 1, 2, 3
 
 class OpCodes:
-    @staticmethod
-    def get_ops():
-        return [
-            OpCodes.addr, OpCodes.addi, OpCodes.mulr, OpCodes.muli, OpCodes.borr, OpCodes.bori,
-            OpCodes.banr, OpCodes.bani, OpCodes.setr, OpCodes.seti, OpCodes.getir, OpCodes.getri,
-            OpCodes.getrr, OpCodes.eqir, OpCodes.eqri, OpCodes.eqrr
-        ]
 
     @staticmethod
     def addr(registers, operation):
@@ -76,6 +71,20 @@ class OpCodes:
     def eqrr(registers, operation):
         registers[operation[C_IDX]] = 1 if registers[operation[A_IDX]] == registers[operation[B_IDX]] else 0
 
+    @staticmethod
+    def get_ops():
+        return OpCodes.operations
+
+    @staticmethod
+    def get_op_index(op):
+        return OpCodes.operations.index(op) if op in OpCodes.operations else -1
+
+OpCodes.operations = [
+    OpCodes.addr, OpCodes.addi, OpCodes.mulr, OpCodes.muli, OpCodes.borr, OpCodes.bori,
+    OpCodes.banr, OpCodes.bani, OpCodes.setr, OpCodes.seti, OpCodes.getir, OpCodes.getri,
+    OpCodes.getrr, OpCodes.eqir, OpCodes.eqri, OpCodes.eqrr
+]
+
 
 # Sample Indices
 REGS_BEFORE_IDX, OPERATION_IDX, REGS_AFTER_IDX = 0, 1, 2
@@ -97,25 +106,67 @@ def extract_samples(lines_iter):
             next(lines_iter)
 
 
-def count_samples_matches(samples):
-    num_sample_matches = list()
+def get_samples_matches(samples):
+    samples_match_ops = list()
     for sample in samples:
-        num_sample_matches.append(0)
+        samples_match_ops.append(set())
         for op in OpCodes.get_ops():
             regs = sample[REGS_BEFORE_IDX].copy()
             op(regs, sample[OPERATION_IDX])
             if regs == sample[REGS_AFTER_IDX]:
-                num_sample_matches[-1] += 1
+                samples_match_ops[-1].add(op)
 
-    return num_sample_matches
+    return samples_match_ops
+
+
+def get_code_to_idx_mapping(op_code_to_possible_indices):
+    code_to_idx = dict()
+
+    num_operations = len(op_code_to_possible_indices)
+    while len(code_to_idx) < num_operations:
+        for op_code in list(op_code_to_possible_indices.keys()):
+            possible_indices = op_code_to_possible_indices[op_code]
+            if len(possible_indices) == 1:
+                code_to_idx[op_code] = possible_indices.pop()
+                del op_code_to_possible_indices[op_code]
+
+                for val in op_code_to_possible_indices.values():
+                    val -= {code_to_idx[op_code]}
+
+    return code_to_idx
+
+
+def execute_program(operations, code_to_op):
+    registers = [0, 0, 0, 0]
+    for op in operations:
+        code_to_op[op[OP_CODE_IDX]](registers, op)
+    return registers
 
 
 if __name__ == '__main__':
-    lines = [l.rstrip() for l in open('../input/day16.in').readlines()]
-    samples = extract_samples(iter(lines))
+    lines = [l.rstrip() for l in sys.stdin.readlines()]
+    it = iter(lines)
+    samples = extract_samples(it)
+    program_operations = [[int(e) for e in l.split(' ')] for l in it if l != '']
 
     ## First part
-    print(sum(map(lambda e: 1 if e >= 3 else 0, count_samples_matches(samples))))
+    sample_matches = get_samples_matches(samples)
+    print(sum(map(lambda e: 1 if len(e) >= 3 else 0, sample_matches)))
 
-    
-    pass
+    ## Get mapping OP_CODE -> operation
+    code_to_ops = dict()
+    for i, matches in enumerate(sample_matches):
+        op_code = samples[i][OPERATION_IDX][OP_CODE_IDX]
+        if op_code not in code_to_ops:
+            code_to_ops[op_code] = matches
+        else:
+            code_to_ops[op_code] = code_to_ops[op_code] & matches
+
+    code_to_possible_indices = {i: {OpCodes.get_op_index(op) for op in code_to_ops[i]} for i in range(len(code_to_ops))}
+
+    code_to_idx = get_code_to_idx_mapping(code_to_possible_indices)
+    code_to_op = {op_code: OpCodes.get_ops()[idx] for op_code, idx in code_to_idx.items()}
+
+    ## Second part
+    final_regs = execute_program(program_operations, code_to_op)
+    print(final_regs[0])
